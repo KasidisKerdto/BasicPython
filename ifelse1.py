c1889afe3046baa2c0ba6adb47960164480f27b0
KasidisKerdto <108352724+KasidisKerdto@users.noreply.github.com>
2022-07-03 16:46:30 +0700

    Add files via upload

---
+++ ifelse1.py
@@ -0,0 +1,16 @@
+#ifelse1.py
+
+money = 200
+if money >=300 :
+    print('กินอาหารญี่ปุ่น')
+#elif money >=150: 
+    #pass
+elif money >=50 :
+    print('กินร้านป้าแดง')
+elif money >0:
+    print('กลับไปกินข้าวที่บ้าน')
+
+else :
+    print('กรุณากรอกเป็นจำนวนเต็ม')
+
+print('กลับมาเรียนต่อ')
